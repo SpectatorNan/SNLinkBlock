cf435ae8cdbe3800f9e46377e79e5a0aaaf0fd93
spectatorMrZ <sepctatornan@gmail.com>
2016-05-29 15:27:50 +0800

    Initial Commit

---
+++ SNLinkBlock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SNLinkBlock
+//
+//  Created by Spectator on 16/5/29.
+//  Copyright © 2016年 Spectator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SNLinkBlock/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SNLinkBlock
+//
+//  Created by Spectator on 16/5/29.
+//  Copyright © 2016年 Spectator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

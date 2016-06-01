10650440b35ec2ed7a7162a80272b88c5e86328b
spectatorNan <spectatornan@gmail.com>
2016-06-02 00:19:06 +0800

    a

--- SNLinkBlock/AppDelegate.m
+++ SNLinkBlock/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSLog(@"hello,world");
+   
     return YES;
 }
 

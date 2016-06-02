595d2f506048d1f772878b7b6cf26987158459a5
spectatorMrZ <sepctatornan@gmail.com>
2016-06-02 16:52:29 +0800

    add searchBar

--- SNLinkBlock/SNLinkBlock/UISearchBar+Link.h
+++ SNLinkBlock/SNLinkBlock/UISearchBar+Link.h
@@ -10,4 +10,24 @@
 
 @interface UISearchBar (Link)
 
+@property (nonatomic, copy, readonly) UISearchBar *(^searchBar_tintColor)(UIColor *color);
+
+@property (nonatomic, copy, readonly) UISearchBar *(^searchBar_barTintColor)(UIColor *color);
+
+@property (nonatomic, copy, readonly) UISearchBar *(^searchBar_searchBarStyle)(UISearchBarStyle searchBarStyle);
+
+@property (nonatomic, copy, readonly) UISearchBar *(^searchBar_barStyle)(UIBarStyle barStyle);
+
+@property (nonatomic, copy, readonly) UISearchBar *(^searchBar_placeholder)(NSString *placeholder);
+
+@property (nonatomic, copy, readonly) UISearchBar *(^searchBar_translucent)(BOOL translucent);
+
+@property (nonatomic, copy, readonly) UISearchBar *(^searchBar_showsScopeBar)(BOOL showsScopeBar);
+
+@property (nonatomic, copy, readonly) UISearchBar *(^searchBar_delegate)(id obj);
+
+@property (nonatomic, copy, readonly) UISearchBar *(^searchBar_cancelTitle)(NSString *title, UIControlState state);
+
+
+@property (nonatomic, copy, readonly) UISearchBar *(^searchBar_showsCancelButton)(BOOL shosCancelButton);
 @end
--- SNLinkBlock/SNLinkBlock/UISearchBar+Link.m
+++ SNLinkBlock/SNLinkBlock/UISearchBar+Link.m
@@ -10,4 +10,126 @@
 
 @implementation UISearchBar (Link)
 
+- (UISearchBar *(^)(UIColor *))searchBar_tintColor {
+    
+    return ^(UIColor *color) {
+        
+        self.tintColor = color;
+        
+        return self;
+    };
+}
+
+- (UISearchBar *(^)(UIColor *))searchBar_barTintColor {
+    
+    return ^(UIColor *color) {
+        
+        self.barTintColor = color;
+        
+        return self;
+    };
+}
+
+- (UISearchBar *(^)(UISearchBarStyle))searchBar_searchBarStyle {
+    
+    return ^(UISearchBarStyle style) {
+        
+        self.searchBarStyle = style;
+        
+        return self;
+    };
+}
+
+
+- (UISearchBar *(^)(UIBarStyle))searchBar_barStyle {
+    
+    return ^(UIBarStyle style) {
+        
+        self.barStyle = style;
+    
+        return self;
+    };
+}
+
+- (UISearchBar *(^)(NSString *))searchBar_placeholder {
+    
+    return ^(NSString *placeholder) {
+        
+        self.placeholder = placeholder;
+        
+        return self;
+    };
+}
+
+
+- (UISearchBar *(^)(BOOL))searchBar_translucent {
+    
+    return ^(BOOL translucent) {
+        
+        self.translucent = translucent;
+        
+        return self;
+    };
+}
+
+- (UISearchBar *(^)(BOOL))searchBar_showsScopeBar {
+    
+    return ^(BOOL showsScopeBar) {
+        
+        self.showsScopeBar = showsScopeBar;
+        
+        return self;
+    };
+}
+
+- (UISearchBar *(^)(id))searchBar_delegate {
+    
+    
+    return ^(id obj) {
+        self.delegate = obj;
+        
+        return self;
+    };
+}
+
+- (UISearchBar *(^)(NSString *, UIControlState))searchBar_cancelTitle {
+    
+    return ^(NSString *title, UIControlState state) {
+        
+        for (id view in self.subviews) {
+            
+            UIView *cancel = (UIView *)view;
+            for (id cc in cancel.subviews) {
+                
+                if ([cc isKindOfClass:[UIButton class]]) {
+                    UIButton *btn = (UIButton*)cc;
+                    [btn setTitle:title forState:state];
+                }
+            }
+        }
+        
+        return self;
+        
+    };
+}
+
+- (UISearchBar *(^)(BOOL))searchBar_showsCancelButton {
+    
+    return ^(BOOL showsCancelButton) {
+        
+        self.showsCancelButton = showsCancelButton;
+        
+        return self;
+    };
+}
+
+
+
+
+
+
+
+
+
+
 @end

a978e58d1add7b57afc5e7243d2e2cb184845984
spectatorMrZ <sepctatornan@gmail.com>
2016-06-07 13:34:05 +0800

    uitextField

--- SNLinkBlock/SNLinkBlock/UITextField+Link.h
+++ SNLinkBlock/SNLinkBlock/UITextField+Link.h
@@ -10,4 +10,25 @@
 
 @interface UITextField (Link)
 
+
+@property (nonatomic, copy, readonly) UITextField *(^tf_placeholder)(NSString *placeholder);
+
+@property (nonatomic, copy, readonly) UITextField *(^tf_delegate)(id target);
+
+@property (nonatomic, copy, readonly) UITextField *(^tf_borderStyle)(UITextBorderStyle borderStyle);
+
+@property (nonatomic, copy, readonly) UITextField *(^tf_backgroundColor)(UIColor *backgroundColor);
+
+@property (nonatomic, copy, readonly) UITextField *(^tf_rightView)(UIView *rightView);
+
+@property (nonatomic, copy, readonly) UITextField *(^tf_leftView)(UIView *leftView);
+
+@property (nonatomic, copy, readonly) UITextField *(^tf_leftViewMode)(UITextFieldViewMode leftViewMode);
+
+@property (nonatomic, copy, readonly) UITextField *(^tf_rightViewMode)(UITextFieldViewMode rightViewMode);
+
+@property (nonatomic, copy, readonly) UITextField *(^tf_contentVerticalAlignment)(UIControlContentVerticalAlignment contentVerticalAlignment);
+
+@property (nonatomic, copy, readonly) UITextField *(^tf_alpha)(CGFloat alpha);
+
 @end
--- SNLinkBlock/SNLinkBlock/UITextField+Link.m
+++ SNLinkBlock/SNLinkBlock/UITextField+Link.m
@@ -10,4 +10,105 @@
 
 @implementation UITextField (Link)
 
+- (UITextField *(^)(id))tf_delegate {
+    
+    return ^(id target) {
+        
+        self.delegate = target;
+        
+        return self;
+    };
+}
+
+
+- (UITextField *(^)(UITextBorderStyle))tf_borderStyle {
+    
+    return ^(UITextBorderStyle borderStyle) {
+        
+        self.borderStyle = borderStyle;
+        
+        return self;
+    };
+}
+
+- (UITextField *(^)(NSString *))tf_placeholder {
+    
+    return ^(NSString *placeholder) {
+        
+        self.placeholder = placeholder;
+        
+        return self;
+    };
+}
+
+- (UITextField *(^)(UIColor *))tf_backgroundColor {
+    
+    return ^(UIColor *backgroundColor) {
+        
+        self.backgroundColor = backgroundColor;
+        
+        return self;
+    };
+}
+
+- (UITextField *(^)(UIView *))tf_rightView {
+    
+    return ^(UIView *rightView ) {
+        
+        self.rightView = rightView;
+        
+        return self;
+    };
+}
+
+- (UITextField *(^)(UIView *))tf_leftView {
+    
+    return ^(UIView *leftView) {
+        
+        self.leftView = leftView;
+        
+        return self;
+    };
+}
+
+- (UITextField *(^)(UITextFieldViewMode))tf_leftViewMode {
+    
+    return ^(UITextFieldViewMode leftViewMode) {
+        
+        self.leftViewMode = leftViewMode;
+        
+        return self;
+    };
+}
+
+- (UITextField *(^)(UITextFieldViewMode))tf_rightViewMode {
+    
+    return ^(UITextFieldViewMode rightViewMode) {
+        
+        self.rightViewMode = rightViewMode;
+        
+        return self;
+    };
+}
+
+- (UITextField *(^)(UIControlContentVerticalAlignment))tf_contentVerticalAlignment {
+    
+    return ^(UIControlContentVerticalAlignment contentVerticalAlignment) {
+        
+        self.contentVerticalAlignment = contentVerticalAlignment;
+        
+        return self;
+    };
+}
+
+- (UITextField *(^)(CGFloat))tf_alpha {
+    
+    return ^(CGFloat alpha) {
+        
+        self.alpha = alpha;
+        
+        return self;
+    };
+}
+
 @end
